eb0e1e7a70b949ea11985bb04795ad2d88b8e1bb
Adam Nording <49994496+AdamNording@users.noreply.github.com>
2020-12-10 12:35:01 +0100

    ReadMe updated and some final touches

--- src/App.js
+++ src/App.js
@@ -3,55 +3,34 @@ import './App.css';
 import { TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 
-
-/**function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Autocomplete
-          id="combo-box-demo"
-          options={programs}
-          getOptionLabel={(option) => option.title}
-          style={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-        />
-      </div>
-    </div>
-  );
-}
-
-const programs = Program;
-console.log(programs);
-
-export default App;
-*/
+/**
+ * This class fetch a get request to localhost:5000/programs. When the requested
+ * data is received it sets the state to response from the server. 
+ * To communicate with the server the proxy (in package.json) is set to the host and port of the 
+ * server.
+ * @author Adam Nording
+ * @date 10/12/2020
+ */
 class App extends Component {
 
   state = {
-    data: []
+    programsData: []
   };
 
   componentDidMount() {
-    console.log("Hejsan");
     this.callBackendApi()
-      .then(res => this.setState({ data: res }))
+      .then(res => this.setState({ programsData: res }))
       .catch(error => console.log(error));
   };
 
   callBackendApi = async () => {
-    const result = [];
     const response = await fetch('/programs');
     const programs = await response.json();
-    console.log("Här är jag" + programs);
-    //const programs = JSON.parse(data);
+
     if (response.status !== 200) {
-      
       throw Error(response.message);
     };
-    /*for (var i in programs){
-      result.push(programs[i]);
-    };*/
-    console.log('RESULTATET: ' + result);
+
     return programs;
   };
 
@@ -61,26 +40,15 @@ class App extends Component {
         <div className="App-header">
           <Autocomplete
             id="combo-box-demo"
-            options={this.state.data}
+            options={this.state.programsData}
             getOptionLabel={(option) => option.title}
             style={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
           />
-          
         </div>
       </div>
     );
   }
 }
 
-
 export default App;
-//<p>{this.state.data}</p>
-
-/*, {
-  headers : { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-   }
-
-}*/
--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
+/**
+ * The server can in this instance only take a get request and send back
+ * an answear. It is listening to port 5000.
+ * @requires JSON-file
+ * @requires express
+ * @author Adam Nording
+ * @date 10/12/2020
+ */
+
 var programs = require('../public/programs.json');
 const express = require('express');
 const app = express();
